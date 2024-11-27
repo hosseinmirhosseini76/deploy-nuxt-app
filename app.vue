2a616ba55415d8310cdcc217bdb7385f460ccfb9
Seyed Hossein Mirhosseini <hosseinmirhosseini76@gmail.com>
2024-11-27 23:19:16 +0330

    feat: adding new section

--- app.vue
+++ app.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
+    <div class="new-changes">
+      This is new changes
+    </div>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div>
 </template>
+
+<style>
+.new-changes {
+  display: flex;
+  justify-content: center;
+  background-color: black;
+  color: #00dd7e;
+  font-size: 3rem;
+  font-weight: 900;
+  padding-top: 2rem;
+}
+</style>
